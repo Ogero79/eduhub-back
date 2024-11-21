95932e62515f16c1ee0734babe5f14fb23cbdbbd
Ogero79 <brianogero@kabarak.ac.ke>
2024-11-21 14:37:03 +0300

    added my git credentials

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ const uploadFileToGitHub = async (fileName, fileBuffer) => {
 
     // Execute Git commands
     execSync('git config --global user.email "brianogero@kabarak.ac.ke"');
-execSync('git config --global user.name "Ogero79"');
+    execSync('git config --global user.name "Ogero79"');
 
     execSync(`git add ${safeFilePath}`);
     console.log(`File added to Git index: ${fileName}`);
